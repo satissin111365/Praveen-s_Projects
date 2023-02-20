ed6a96820258d35bd837566131c212a3e09609fa
praveenapalla <praveenapalla2000@gmail.com>
2023-02-20 10:35:16 +0530

    created formpage

--- Employee Form/FormPage/src/app/form-component/form-component.component.ts
+++ Employee Form/FormPage/src/app/form-component/form-component.component.ts
@@ -1,60 +1,60 @@
-import { ReturnStatement } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-form-component',
   templateUrl: './form-component.component.html',
   styleUrls: ['./form-component.component.css']
 })
 export class FormComponentComponent {
- 
-  
-  formcomponent= new FormGroup({
-      firstname: new FormControl("",[Validators.required,Validators.minLength(6),Validators.pattern("[a-zA-Z].*")]),
-      lastname: new FormControl("",[Validators.required,Validators.minLength(6),Validators.pattern("[a-zA-Z].*")]),
-      email: new FormControl("",[Validators.required, Validators.email]),
-      empid: new FormControl("",[Validators.required,Validators.pattern("[0-9]*"),Validators.minLength(6),Validators.maxLength(6)]),
-      mobile: new FormControl("",[Validators.required,Validators.pattern("[0-9]*"),Validators.minLength(10),Validators.maxLength(10)]),
-      gender: new FormControl("")
+  formcomponent = new FormGroup({
+    firstname: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern('[a-zA-Z].*')]),
+    lastname: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern('[a-zA-Z].*')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    empid: new FormControl('', [Validators.required, Validators.pattern('[0-9]*'), Validators.minLength(6), Validators.maxLength(6)]),
+    mobile: new FormControl('', [Validators.required, Validators.pattern('[0-9]*'), Validators.minLength(10), Validators.maxLength(10)]),
+    gender: new FormControl('')
+  });
 
-    });
-  userForm: any;
-  firstname: any;
-    setValue() {
-      this.firstname=this.userForm.get('first_name')?.value; // input value retrieved
-    }
-    employeesubmitted(){
-      
-      
- 
-      console.log(this.formcomponent);
-    }
-    get FirstName() : FormControl{
-      return this.formcomponent.get("firstname") as FormControl;
-    }
-    get LastName() : FormControl{
-      return this.formcomponent.get("lastname") as FormControl;
-    }
-    get Email() : FormControl{
-      return this.formcomponent.get("email") as FormControl;
-    }
-    get Empid() : FormControl{
-      return this.formcomponent.get("empid") as FormControl;
-    }
-    get Mobile() : FormControl{
-      return this.formcomponent.get("mobile") as FormControl;
-    }
-    get Gender() : FormControl{
-      return this.formcomponent.get("gender") as FormControl;
-    }
-    newEmployee = { firstname: '', lastname: '', email: '', empid:'', mobile:'', gender:''  };
   employeeList: any[] = [];
 
+  constructor(private formBuilder: FormBuilder) {}
+
   Submit() {
-    this.employeeList.push(this.newEmployee);
-    this.newEmployee = { firstname: '', lastname: '', email: '', empid:'', mobile:'', gender:'' };
+    const newEmployee = {
+      firstname: this.formcomponent.get('firstname')?.value,
+      lastname: this.formcomponent.get('lastname')?.value,
+      email: this.formcomponent.get('email')?.value,
+      empid: this.formcomponent.get('empid')?.value,
+      mobile: this.formcomponent.get('mobile')?.value,
+      gender: this.formcomponent.get('gender')?.value,
+    };
+
+    this.employeeList.push(newEmployee);
+    this.formcomponent.reset();
   }
 
- 
+  get FirstName(): FormControl {
+    return this.formcomponent.get('firstname') as FormControl;
   }
+
+  get LastName(): FormControl {
+    return this.formcomponent.get('lastname') as FormControl;
+  }
+
+  get Email(): FormControl {
+    return this.formcomponent.get('email') as FormControl;
+  }
+
+  get Empid(): FormControl {
+    return this.formcomponent.get('empid') as FormControl;
+  }
+
+  get Mobile(): FormControl {
+    return this.formcomponent.get('mobile') as FormControl;
+  }
+
+  get Gender(): FormControl {
+    return this.formcomponent.get('gender') as FormControl;
+  }
+}
